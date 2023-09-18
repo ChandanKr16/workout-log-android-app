6ed884ce3aced265c7bdc0973c4f5aa883d0f992
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-09-18 11:25:16 +0530

    Created Activity for Weight Tracking

---
+++ app/src/main/java/me/chandankumar/workouttracker/ui/WeightLogActivity.java
@@ -0,0 +1,16 @@
+package me.chandankumar.workouttracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.chandankumar.workouttracker.R;
+
+public class WeightLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight_log);
+    }
+}
