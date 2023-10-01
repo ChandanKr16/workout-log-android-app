9e2ee2710a3510cbdad1b2a2f4285c507a821d36
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-10-01 10:33:07 +0530

    Updated MainActivity and HomeActivity

---
+++ app/src/main/java/me/chandankumar/workouttracker/ui/HomeActivity.java
@@ -0,0 +1,26 @@
+package me.chandankumar.workouttracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import me.chandankumar.workouttracker.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void showWorkoutLogActivity(View view){
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void showWeightLogActivity(View view) {
+        startActivity(new Intent(this, WeightLogActivity.class));
+    }
+}
--- app/src/main/java/me/chandankumar/workouttracker/ui/SplashActivity.java
+++ app/src/main/java/me/chandankumar/workouttracker/ui/SplashActivity.java
@@ -12,7 +12,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        startActivity(new Intent(SplashActivity.this, HomeActivity.class));
         finish();
 
     }
